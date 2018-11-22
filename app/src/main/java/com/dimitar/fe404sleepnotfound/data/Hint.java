30dbf94129217ac2add3ac4867175e135c4e7c7c
DesJC <jcd9930@hotmail.ca>
2018-11-22 11:07:09 -0500

    Add data class for hint

---
+++ app/src/main/java/com/dimitar/fe404sleepnotfound/data/Hint.java
@@ -0,0 +1,16 @@
+package com.dimitar.fe404sleepnotfound.data;
+
+/**
+ * Visual Hint, the hint is written in the image,
+ * there also is a source supporting the hint
+ */
+public final class Hint {
+    /**
+     * Path pointing to the image file
+     */
+    public String img;
+    /**
+     * Points to a website which is the source of this Hint
+     */
+    public String url;
+}
