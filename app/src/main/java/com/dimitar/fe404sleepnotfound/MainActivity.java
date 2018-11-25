afeb7aba9db78be2544d0c32c041eff6d76ba766
unknown <jamroa.jr@gmail.com>
2018-11-25 00:04:08 -0500

    empty exchange activity

--- app/src/main/java/com/dimitar/fe404sleepnotfound/MainActivity.java
+++ app/src/main/java/com/dimitar/fe404sleepnotfound/MainActivity.java
@@ -1,13 +1,31 @@
 package com.dimitar.fe404sleepnotfound;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+
+    Button activityBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        activityBtn = findViewById(R.id.Temp);
+        activityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(this, DisplayMessageActivity.class);
+            }
+        });
+
+
     }
 }
