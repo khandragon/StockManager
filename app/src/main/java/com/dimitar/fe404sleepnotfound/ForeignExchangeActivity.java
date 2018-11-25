07f95a17680b772e91df8e7fc0b7b1a5b061edf6
unknown <jamroa.jr@gmail.com>
2018-11-25 12:02:15 -0500

    Working currency list json get

--- app/src/main/java/com/dimitar/fe404sleepnotfound/ForeignExchangeActivity.java
+++ app/src/main/java/com/dimitar/fe404sleepnotfound/ForeignExchangeActivity.java
@@ -2,8 +2,6 @@ package com.dimitar.fe404sleepnotfound;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.widget.TextView;
 
 import com.squareup.okhttp.Callback;
@@ -11,16 +9,20 @@ import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
+import org.json.JSONObject;
+
 import java.io.IOException;
 
 public class ForeignExchangeActivity extends Activity {
 
     String currencyListUrl = "https://openexchangerates.org/api/currencies.json";
-    Handler currencyHandles;
-
+    String currencyListString;
+    JSONObject currencyJson;
     //URL for the open exchange api with the api key attached;
     //Unable to change the base type with a free api key. Will be USD
     String openExchangeUrl = "https://openexchangerates.org/api/latest.json?app_id=7b0dc9c3d41b40989a3bafcea4683d5f";
+    String openExchangeString;
+    JSONObject openExchangeJson;
 
     TextView text;
     String textContent;
@@ -31,10 +33,12 @@ public class ForeignExchangeActivity extends Activity {
         setContentView(R.layout.activity_foreignexchange);
 
         text = findViewById(R.id.textView);
+        currencyListGet();
+        openExchangeGet();
+    }
 
-
+    private void currencyListGet(){
         OkHttpClient client = new OkHttpClient();
-        currencyHandles = new Handler(Looper.getMainLooper());
         Request currencyListRequest = new Request.Builder()
                 .url(currencyListUrl)
                 .build();
@@ -48,14 +52,58 @@ public class ForeignExchangeActivity extends Activity {
 
             @Override
             public void onResponse(final Response response) throws IOException {
-                currencyHandles.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        text.setText(response.body().toString());
+                if(response.isSuccessful()){
+                    try{
+                        currencyListString = response.body().string();
+                        currencyJson = new JSONObject(currencyListString);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try{
+                                    text.setText(currencyJson.get("CAD").toString());
+                                }catch (Exception e){
+                                    text.setText("Failed to find CAD");
+                                }
+                            }
+                        });
+                    }catch (Exception e){
+                        text.setText("Faliled to get currency list");
+
                     }
-                });
+                }else {
+                    text.setText("Faliled to get currency list");
+                }
             }
         });
+    }
 
+    private void openExchangeGet(){
+        OkHttpClient client = new OkHttpClient();
+        Request currencyListRequest = new Request.Builder()
+                .url(currencyListUrl)
+                .build();
+
+        client.newCall(currencyListRequest).enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+                System.out.println("Fuck fuck fuck fuck fuck");
+                textContent = "Fick adfsadfasdasd";
+            }
+
+            @Override
+            public void onResponse(final Response response) throws IOException {
+                if(response.isSuccessful()){
+                    currencyListString = response.body().string();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            text.setText(currencyListString);
+                        }
+                    });
+                }else {
+                    text.setText("Faliled to get currency list");
+                }
+            }
+        });
     }
 }
