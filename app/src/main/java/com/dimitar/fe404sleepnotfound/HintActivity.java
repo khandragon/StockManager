94b4346e2f015dd112b1188193d4c6e202811e4f
DesJC <jcd9930@hotmail.ca>
2018-11-30 20:51:51 -0500

    Add shuffling of hints, internet source displaying for a hint and a download animation

--- app/src/main/java/com/dimitar/fe404sleepnotfound/HintActivity.java
+++ app/src/main/java/com/dimitar/fe404sleepnotfound/HintActivity.java
@@ -1,7 +1,9 @@
 package com.dimitar.fe404sleepnotfound;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.drawable.AnimationDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageButton;
@@ -15,6 +17,8 @@ import com.dimitar.fe404sleepnotfound.persistence.HintDAOFirebase;
 import com.google.firebase.auth.AuthResult;
 
 import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.IntStream;
 
 public final class HintActivity extends MenuActivity {
     private final static String TAG = "HintActivity";
@@ -56,6 +60,7 @@ public final class HintActivity extends MenuActivity {
      * @param result
      */
     private void onLoginSuccess(AuthResult result) {
+        Log.d(TAG, "Successful Login!!");
         if(hints != null) {
             Log.d(TAG, "onLoginSuccess : hints are downloaded");
             setupHintCircularArray();
@@ -80,8 +85,8 @@ public final class HintActivity extends MenuActivity {
      */
     private void initHints(Hint[] hintArr) {
         // init the hint circular array
-        shuffleArray(hintArr);
-        Log.d(TAG, Arrays.toString(hintArr));
+        hintArr = shuffleArray(hintArr);
+        Log.d(TAG, "Received Hints : " + Arrays.toString(hintArr));
         hints = new CircularArray<>(hintArr);
 
         setupHintCircularArray();
@@ -125,12 +130,43 @@ public final class HintActivity extends MenuActivity {
     }
 
     /**
-     * Shuffles the array on itself
+     * Shuffles the array using the Fisher-Yates algorithm
      *
      * @param array array to shuffle
      * @param <T> type of elements in the array
      */
-    private <T> void shuffleArray(T[] array) {
+    private <T> T[] shuffleArray(T[] array) {
+        T[] outArray = (T[]) new Object[array.length];
+
+        int[] scratch = range(0, array.length);
+        int limit = array.length - 1;
+        Random rand = new Random();
+
+        int toPick;
+        for(int i = 0; i < outArray.length; i++, limit--) {
+            toPick = rand.nextInt(limit + 1);
+            outArray[i] = array[scratch[toPick]];
+
+            permute(scratch, toPick, limit);
+        }
+
+        return outArray;
+    }
+
+    private int[] range(int from, int to) {
+        int[] array = new int[to - from];
+
+        for(int i = 0; i < array.length; i++) {
+            array[i] = from++;
+        }
+
+        return array;
+    }
+
+    private void permute(int[] array, int indexA, int indexB) {
+        int temp = array[indexA];
+        array[indexA] = array[indexB];
+        array[indexB] = temp;
     }
 
     /**
@@ -139,7 +175,8 @@ public final class HintActivity extends MenuActivity {
      * @param hint the hint providing the url
      */
     private void openHintSource(Hint hint) {
-        Toast.makeText(getApplicationContext(), hint.url, Toast.LENGTH_SHORT).show();
+        Intent httpIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(hint.url));
+        startActivity(httpIntent);
     }
 
     private void startDownloadAnimation() {
