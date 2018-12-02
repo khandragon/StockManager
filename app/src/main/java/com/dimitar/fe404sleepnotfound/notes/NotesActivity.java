9baa347be1a97a581bd767d80b5d85b6b91842ce
khandragon <saaadkhan23@yahoo.ca>
2018-12-01 19:16:49 -0500

    try to fix layouts and recyclerview

--- app/src/main/java/com/dimitar/fe404sleepnotfound/notes/NotesActivity.java
+++ app/src/main/java/com/dimitar/fe404sleepnotfound/notes/NotesActivity.java
@@ -1,24 +1,11 @@
 package com.dimitar.fe404sleepnotfound.notes;
 
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
+
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.ScrollView;
-import android.widget.TextView;
+
 
 import com.dimitar.fe404sleepnotfound.R;
 import com.dimitar.fe404sleepnotfound.menu.MenuActivity;
-
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import javax.net.ssl.HttpsURLConnection;
 
 
 public class NotesActivity extends MenuActivity {
@@ -27,6 +14,10 @@ public class NotesActivity extends MenuActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
+        RecyclerView recyclerView = findViewById(R.id.notesList);
+        final NotesListAdapter adapter = new NotesList(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
 
---
+++ app/src/main/java/com/dimitar/fe404sleepnotfound/notes/NotesListAdapter.java
@@ -0,0 +1,56 @@
+package com.dimitar.fe404sleepnotfound.notes;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.dimitar.fe404sleepnotfound.R;
+import com.dimitar.fe404sleepnotfound.notes.data.Note;
+
+import java.util.Collections;
+import java.util.List;
+
+public class NotesListAdapter extends RecyclerView.Adapter<NotesListAdapter.NotesViewHolder> {
+
+    class NotesViewHolder extends RecyclerView.ViewHolder {
+        private final TextView NotesItemView;
+
+        private NotesViewHolder(View itemView) {
+            super(itemView);
+            NotesItemView = itemView.findViewById(R.id.note_item);
+        }
+    }
+
+    private final LayoutInflater mInflater;
+    private List<Note> mNotes = Collections.emptyList(); // Cached copy of Notes
+
+    public NotesListAdapter(Context context) {
+        mInflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public NotesViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = mInflater.inflate(R.layout.recyclerview, parent, false);
+        return new NotesViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(NotesViewHolder holder, int position) {
+        Note current = mNotes.get(position);
+        holder.NotesItemView.setText(current.getText());
+    }
+
+    public void setNotes(List<Note> Notes) {
+        mNotes = Notes;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNotes.size();
+    }
+}
+
+
